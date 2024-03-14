506f3b758fc47b24ace87428928a383e065d4947
mustafa <msolmaz83@gmail.com>
2024-03-14 23:41:47 +0100

    index_app

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const PORT = process.env.PORT || 8003
 
 app.use(express.json());
 
+app.all("/", (req, res) => {
+    res.send({
+      error: false,
+      message: "Welcome to RENT A CAR API",
+      documents: {
+        swagger: "/documents/swagger",
+        redoc: "/documents/redoc",
+        json: "/documents/json",
+      },
+      user: req.user,
+    });
+  });
+  
+
 require("./src/configs/db")
 app.use(require("./src/middelerwares/loger"))
 app.use(require("./src/middelerwares/authentication"))
@@ -18,4 +32,4 @@ app.use("/images",express.static("./uploads"))
 
 // Bu genellikle resim dosyaları, stil dosyaları veya istemci tarafından talep edilen diğer statik içerikleri sunucunun dışında bir klasörde depolamak için kullanılır. Bu, sunucunun daha hafif olmasına ve talep edilen dosyaların doğrudan sunucu tarafından sağlanmasına olanak tanır, böylece her istek için dinamik içerik üretme ihtiyacı ortadan kalkar
 
-app.listen(PORT,()=>console.log("indexteyiz"))
+app.listen(PORT,()=>console.log("index"))
